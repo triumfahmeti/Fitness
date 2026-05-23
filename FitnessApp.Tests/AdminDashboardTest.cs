cc77f99206767a24b9110be4286a7bb2cdf4aa32
erahema70368 <eh70368@ubt-uni.net>
2026-05-23 23:18:04 +0200

    Test CI/CD

--- FitnessApp.Tests/AdminDashboardTest.cs
+++ FitnessApp.Tests/AdminDashboardTest.cs
@@ -37,7 +37,7 @@ namespace FitnessApp.Tests
             {
                 _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
 
-driver.Navigate().GoToUrl("http://127.0.0.1:4173/login");
+                driver.Navigate().GoToUrl("http://127.0.0.1:5173/login");
                 driver.FindElement(By.Id("email")).SendKeys("admin1@gmail.com");
                 driver.FindElement(By.Id("password")).SendKeys("Admin12.");
                 driver.FindElement(By.Id("login-button")).Click();
@@ -78,9 +78,9 @@ driver.Navigate().GoToUrl("http://127.0.0.1:4173/login");
                     Assert.Fail($"Login failed before dashboard assertions. UI error: {errorText}");
                 }
 
-driver.Navigate().GoToUrl("http://127.0.0.1:4173/admin");
+                driver.Navigate().GoToUrl("http://127.0.0.1:5173/admin");
                 _wait.Until(d => !d.PageSource.Contains("Loading..."));
-    //testt
+
                 // titulli
                 Assert.True(
                     _wait.Until(d => d.FindElements(By.XPath("//h2[normalize-space()='Dashboard']")).Any()),
@@ -90,7 +90,7 @@ driver.Navigate().GoToUrl("http://127.0.0.1:4173/admin");
                 // Total Users karta
                 Assert.True(
                     _wait.Until(d => d.FindElements(By.XPath("//div[contains(@class,'card-body')]//*[normalize-space()='Total Users']")).Any()),
-                    "Total   Users card was not found."
+                    "Total Users card was not found."
                 );
 
                 // Kartat e grafikave
