b0514ba347857ff6b58b0e6e0a865d56dbb094e2
triumfahmeti <ta70227@ubt-uni.net>
2026-06-18 14:31:26 +0200

    axios

--- Frontend/frontend/src/api/axios.js
+++ Frontend/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7103",
+  baseURL: "https://localhost:7103",
   // withCredentials: true, // only if you plan to use cookies
 });
 
