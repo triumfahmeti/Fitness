45650597ffc50b6eb37c86315ae47d9b74c45a09
triumfahmeti <ta70227@ubt-uni.net>
2026-06-18 14:37:46 +0200

    fix

--- FitnessApp.Tests/obj/Debug/net8.0/FitnessApp.Tests.AssemblyInfo.cs
+++ FitnessApp.Tests/obj/Debug/net8.0/FitnessApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b866a7798cf4f981d24c5c881cfa264a1b1d4f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1e01e3b4a89c00679a257cf70cf3e829e7d0a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net9.0/Fitness.AssemblyInfo.cs
+++ obj/Debug/net9.0/Fitness.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058f8918fe3f9d5462e4a0e592e4a49d664411fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1e01e3b4a89c00679a257cf70cf3e829e7d0a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
